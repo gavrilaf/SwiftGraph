06c09ca1eb0b41a2fe5b9c1f96c39ac5f6bde2e4
Eugen Fedchenko <eugenf78@gmail.com>
2017-09-03 12:20:32 +0300

    refactoring

---
+++ Sources/Graph/Base.swift
@@ -0,0 +1,9 @@
+//
+//  Base.swift
+//  SwiftGraph
+//
+//  Created by Eugen Fedchenko on 9/3/17.
+//
+//
+
+import Foundation
---
+++ Sources/Graph/SimpleGraph.swift
@@ -0,0 +1,9 @@
+//
+//  SimpleGraph.swift
+//  SwiftGraph
+//
+//  Created by Eugen Fedchenko on 9/3/17.
+//
+//
+
+import Foundation
